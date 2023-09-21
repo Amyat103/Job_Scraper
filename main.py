8aa8ccc4ab91e5ac74fde1f1bc9ee5036bccef96
Amyat103 <zhoudavid7312@gmail.com>
2023-09-21 10:52:13 -0400

    added find_all for estimated salary and location

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import key
 from bs4 import BeautifulSoup
 
+# TODO: finish adding attributes, discription, and date posted
+
 #requests
 r = requests.get(f"https://api.scrapingant.com/v2/general?url=https%3A%2F%2Fwww.indeed.com%2Fjobs%3Fq%3DPython%2BInternship%26l%3DBoston%252C%2BMA%26start%3D10%26pp%3DgQAPAAABimfNo-sAAAACESYLbQAcAQEBCBbB6hjtFyr5Z2-Pg3aki9oX2Kf9o1NJ-AAA%26vjk%3D66192889ae4b5af1&x-api-key={key.API_KEY}")
 print(r.status_code) #keep till after changing link
@@ -16,7 +18,21 @@ for title in titles:
     print(title.text)
 
 print()
+
 #find company name
 company_names = soup.find_all("span", class_="companyName")
 for name in company_names:
     print(name.text)
+
+print()
+#find company location
+company_locations = soup.find_all("div", class_="companyLocation")
+for location in company_locations:
+    print(location.text)
+
+print()
+#find estimate salary by indeed
+estimate_salaries = soup.find_all("span", class_="estimated-salary")
+for salary in estimate_salaries:
+    print(salary.text)
+

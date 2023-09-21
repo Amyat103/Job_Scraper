05640d48896f6e3e8267675ad6e144a65c6427ea
Amyat103 <zhoudavid7312@gmail.com>
2023-09-21 11:05:16 -0400

    added todo

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import requests
 import key
 from bs4 import BeautifulSoup
 
-# TODO: finish adding attributes, discription, and date posted
+# TODO: finish adding attributes, discription, date posted, and link
+# TODO: Pair all the info to each company
+# TODO: Put them into excel
 
 #requests
 r = requests.get(f"https://api.scrapingant.com/v2/general?url=https%3A%2F%2Fwww.indeed.com%2Fjobs%3Fq%3DPython%2BInternship%26l%3DBoston%252C%2BMA%26start%3D10%26pp%3DgQAPAAABimfNo-sAAAACESYLbQAcAQEBCBbB6hjtFyr5Z2-Pg3aki9oX2Kf9o1NJ-AAA%26vjk%3D66192889ae4b5af1&x-api-key={key.API_KEY}")

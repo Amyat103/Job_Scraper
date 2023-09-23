a6698da445aab9e5650ee66ea6ee50ec60de4de3
Amyat103 <zhoudavid7312@gmail.com>
2023-09-22 20:51:41 -0400

    added find_all for job_attributes and description_snippet

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import requests
 import key
 from bs4 import BeautifulSoup
 
-# TODO: finish adding attributes, discription, date posted, and link
+# TODO: finish adding snippet, discription, date posted, and link
 # TODO: Pair all the info to each company
 # TODO: Put them into excel
+# TODO: Put everything in functions
+
 
 #requests
 r = requests.get(f"https://api.scrapingant.com/v2/general?url=https%3A%2F%2Fwww.indeed.com%2Fjobs%3Fq%3DPython%2BInternship%26l%3DBoston%252C%2BMA%26start%3D10%26pp%3DgQAPAAABimfNo-sAAAACESYLbQAcAQEBCBbB6hjtFyr5Z2-Pg3aki9oX2Kf9o1NJ-AAA%26vjk%3D66192889ae4b5af1&x-api-key={key.API_KEY}")
@@ -38,3 +40,14 @@ estimate_salaries = soup.find_all("span", class_="estimated-salary")
 for salary in estimate_salaries:
     print(salary.text)
 
+print()
+#find attribute snippet
+attribute_snippets = soup.find_all("div", class_="attribute_snippet")
+for attribute in attribute_snippets:
+    print(snippet.text)
+
+print()
+#find description
+description_snippets = soup.find_all("div", class_="job-snippet")
+for description in description_snippets:
+    print(description.text)

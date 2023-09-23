66ec234b26035086b6f0e2d2bfae484ced4bad0e
Amyat103 <zhoudavid7312@gmail.com>
2023-09-22 23:37:32 -0400

    added job_type and location to change link.

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ import pprint
 
 
 #requests
-r = requests.get(f"https://api.scrapingant.com/v2/general?url=https%3A%2F%2Fwww.indeed.com%2Fjobs%3Fq%3DPython%2BInternship%26l%3DBoston%252C%2BMA%26start%3D10%26pp%3DgQAPAAABimfNo-sAAAACESYLbQAcAQEBCBbB6hjtFyr5Z2-Pg3aki9oX2Kf9o1NJ-AAA%26vjk%3D66192889ae4b5af1&x-api-key={key.API_KEY}")
+job_type = "Python Internship"
+location = "Seattle WA"
+link = f"https://api.scrapingant.com/v2/general?url=https%3A%2F%2Fwww.indeed.com%2Fjobs%3Fq%3D{job_type}%26l%3D{location}%26start%3D10%26pp%3DgQAPAAABimfNo-sAAAACESYLbQAcAQEBCBbB6hjtFyr5Z2-Pg3aki9oX2Kf9o1NJ-AAA%26vjk%3D66192889ae4b5af1&x-api-key={key.API_KEY}"
+r = requests.get(link)
 print(r.status_code) #keep till after changing link
 response = r.content
 

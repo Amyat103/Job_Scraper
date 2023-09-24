fe46871fa507be9b1e4b9e72bd40bd0da32ebab0
Amyat103 <zhoudavid7312@gmail.com>
2023-09-23 22:38:17 -0400

    fixed put job title inside loop. added page loop to search multiple pages

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ import pandas as pd
 # TODO: Put everything in functions
 # TODO: Make it search multiple pages
 
-
 #requests
 job_type = "Python Internship"
 location = "Seattle WA"
-link = f"https://api.scrapingant.com/v2/general?url=https%3A%2F%2Fwww.indeed.com%2Fjobs%3Fq%3D{job_type}%26l%3D{location}%26start%3D10%26pp%3DgQAPAAABimfNo-sAAAACESYLbQAcAQEBCBbB6hjtFyr5Z2-Pg3aki9oX2Kf9o1NJ-AAA%26vjk%3D66192889ae4b5af1&x-api-key={key.API_KEY}"
+link = f"https://api.scrapingant.com/v2/general?url=https%3A%2F%2Fwww.indeed.com%2Fjobs%3Fq%3D{job_type}%26l%3D{location}%26start%3D{page}%26pp%3DgQAPAAABimfNo-sAAAACESYLbQAcAQEBCBbB6hjtFyr5Z2-Pg3aki9oX2Kf9o1NJ-AAA%26vjk%3D66192889ae4b5af1&x-api-key={key.API_KEY}"
 r = requests.get(link, timeout=60)
 print(r.status_code) #keep till after changing link
 response = r.content
@@ -19,10 +18,7 @@ response = r.content
 #parse
 soup = BeautifulSoup(response, "lxml")
 
-#find job title
-titles = soup.find_all("h2", class_="jobTitle")
-for title in titles:
-    print(title.text)
+
 
 print()
 
@@ -32,6 +28,7 @@ def find_job(soup):
     job_boxes = soup.find_all("div", class_="cardOutline")
     result = []
     for job in job_boxes:
+        title = soup.find("h2", class_="jobTitle").text
         company_names = job.find("span", class_="companyName").text
         company_locations = job.find("div", class_="companyLocation").text
         try:
@@ -51,6 +48,7 @@ def find_job(soup):
         link_head = "https://www.indeed.com/viewjob"
         link = link_head + end_link
         result_sin = {
+            "Job Title": title,
             "Company Name": company_names,
             "Job Location": company_locations,
             "Estimated Salary": estimate_salaries,
@@ -63,6 +61,10 @@ def find_job(soup):
 
     return result
 
+for page in range(0, 50, 10):
+
+
+
 job_result = find_job(soup)
 pprint.pprint(job_result, sort_dicts=False)
 
@@ -70,7 +72,12 @@ df = pd.DataFrame(job_result)
 df.to_excel("result.xlsx", index=False)
 
 print(df)
+print(link)
 
+# #find job title
+# titles = soup.find_all("h2", class_="jobTitle")
+# for title in titles:
+#     print(title.text)
 # #find company name
 # company_names = soup.find_all("span", class_="companyName")
 # for name in company_names:

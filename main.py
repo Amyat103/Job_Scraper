55a97439d8ac8b1c18aca5a4acd7c4022fe5a64a
Amyat103 <zhoudavid7312@gmail.com>
2023-09-24 16:23:33 -0400

    increased timeout, fixed job title being all the same.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def find_job(soup):
     job_boxes = soup.find_all("div", class_="cardOutline")
     result = []
     for job in job_boxes:
-        title = soup.find("h2", class_="jobTitle").text
+        title = job.find("h2", class_="jobTitle").text
         company_names = job.find("span", class_="companyName").text
         company_locations = job.find("div", class_="companyLocation").text
         try:
@@ -44,9 +44,10 @@ def find_job(soup):
 
 def request(page, location, job_type):
     link = f"https://api.scrapingant.com/v2/general?url=https%3A%2F%2Fwww.indeed.com%2Fjobs%3Fq%3D{job_type}%26l%3D{location}%26start%3D{page}%26pp%3DgQAPAAABimfNo-sAAAACESYLbQAcAQEBCBbB6hjtFyr5Z2-Pg3aki9oX2Kf9o1NJ-AAA%26vjk%3D66192889ae4b5af1&x-api-key={key.API_KEY}"
-    r = requests.get(link, timeout=120)
+    r = requests.get(link, timeout=200)
     response = r.content
     soup = BeautifulSoup(response, "lxml")
+    print(link)
     return soup
 
 
@@ -60,8 +61,8 @@ def main():
     # final list
     job_result = []
     job_type = "Python Internship"
-    location = "Boston MA"
-    for page in range(0, 50, 10):
+    location = "San Francisco CA"
+    for page in range(0, 30, 10):
         soup = request(page, location, job_type)
         page_result = find_job(soup)
         job_result.extend(page_result)
